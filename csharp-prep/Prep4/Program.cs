2ab75b63634a5da784c3adacf9b4587394783338
mattmabra60 <mattmabra60@gmail.com>
2025-01-16 16:19:58 -0700

    finished prep 4 and did some of the sandbox activities with functions and working with lists

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,57 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number = -1; 
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (number != 0)
+        {   
+            Console.Write("Enter a number: ");
+            string response = Console.ReadLine();
+            number = int.Parse(response);
+
+            if (number == 0)
+            {
+                break;
+            }
+            numbers.Add(number);
+        }
+        // sum the numbers
+        int sum = 0;
+        foreach (int num in numbers)
+        {   
+            sum += num;
+        }
+        
+        Console.WriteLine("The sum of the numbers is: " + sum);
+        // average the numbers
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine("The average of the numbers is: " + (average));
+
+        // find the largest number
+        int largest = numbers[0];
+
+        foreach (int num in numbers)
+        {
+            if (num > largest)
+            {
+                largest = num;
+            }
+        }
+        Console.WriteLine("The largest number is: " + largest);
+
+        // sort the list
+        List<int> list_of_numbers = numbers;
+
+        list_of_numbers.Sort();
+
+        Console.WriteLine("The sorted list of numbers is: " + string.Join(", ", list_of_numbers));
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,57 @@
 using System;
+using System.Globalization;
+using System.Net.Http.Headers;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // this is a comment
+        string adjective = GetAdjective();
+        string noun = GetNoun();
+
+        int number = Multiply(3, 4);
+        Console.WriteLine($"I looked out the window and saw {number} {adjective} {noun}s.");
+    }
+
+    static int Multiply(int number1, int number2)
+    {
+        int product = number1 * number2;
+        return product;
+    }
+    static string GetAdjective()
+    {
+        List<string> words = new List<string>();
+        words.Add("red");
+        words.Add("blue");
+        words.Add("green");
+        words.Add("yellow");
+        words.Add("big");
+        words.Add("small");
+
+        string adjective = words[2];
+
+        return adjective;
+    }   
+
+    static string GetNoun()
+    {
+        string noun = "bird";
+
+        return noun;
+    }
+
+    static string GetAmount(int number)
+    { 
+        string amount = "";
+        if (number > 1)
+        {
+            amount = "s";
+        }
+        else 
+        {
+            amount = "";
+        }
+        return amount;
     }
 }
