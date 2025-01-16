0319bdc44e6f1cfe429b1b84333732856a73434f
mattmabra60 <mattmabra60@gmail.com>
2025-01-16 14:24:22 -0700

    finished the Learning Activity - C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        int response = int.Parse(Console.ReadLine());
+        int guess;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+        
+            if (response > guess)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else if (response < guess)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else
+            {
+                Console.WriteLine("You got it!");
+            }
+        }while (response != guess);
+        
     }
 }
