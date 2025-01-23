74e47fdd17940a1283ef316281ed6673fb8e571f
mattmabra60 <mattmabra60@gmail.com>
2025-01-22 21:02:02 -0700

    finished the W03 prepare assignment

---
+++ sandbox/Sandbox/Movie.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.Design.Serialization;
+
+public class Movie
+{
+    public string _title;
+    public int _year;
+    public int _runtime;
+    public string _rating;
+
+    public void Display()
+    {
+        Console.WriteLine(_title);
+    }
+}
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,52 +6,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        // this is a comment
-        string adjective = GetAdjective();
-        string noun = GetNoun();
+        Movie favoriteMovie = new Movie();
+        favoriteMovie._title = "Star wars";
+        favoriteMovie._year = 1977;
+        favoriteMovie._runtime = 150;
+        favoriteMovie._rating = "PG";
 
-        int number = Multiply(3, 4);
-        Console.WriteLine($"I looked out the window and saw {number} {adjective} {noun}s.");
-    }
+        Movie otherMovie = new Movie();
+        otherMovie._title = "Avatar";
+        otherMovie._year = 2009;
+        otherMovie._runtime = 162;
+        otherMovie._rating = "PG-13";
 
-    static int Multiply(int number1, int number2)
-    {
-        int product = number1 * number2;
-        return product;
-    }
-    static string GetAdjective()
-    {
-        List<string> words = new List<string>();
-        words.Add("red");
-        words.Add("blue");
-        words.Add("green");
-        words.Add("yellow");
-        words.Add("big");
-        words.Add("small");
+        favoriteMovie.Display();
 
-        string adjective = words[2];
+        DisplayMovie(favoriteMovie);
+        DisplayMovie(otherMovie);
 
-        return adjective;
-    }   
+        Console.WriteLine("My favorite movie is " + favoriteMovie._title + ".");
 
-    static string GetNoun()
-    {
-        string noun = "bird";
-
-        return noun;
-    }
-
-    static string GetAmount(int number)
-    { 
-        string amount = "";
-        if (number > 1)
+        static void DisplayMovie(Movie aMovie)
         {
-            amount = "s";
+            Console.WriteLine($"{aMovie._title} - {aMovie._year}");
         }
-        else 
-        {
-            amount = "";
-        }
-        return amount;
     }
 }
