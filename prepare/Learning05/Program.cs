89a7cc1ad38fd5a42d8510d0ba499c2d7999fc91
mattmabra60 <mattmabra60@gmail.com>
2025-04-03 14:59:14 -0600

    Prepare Learning Activity Polymorphism

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Yellow", 5);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Orange", 2, 6);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Blue", 7);
+        shapes.Add(s3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
